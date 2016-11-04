fccb2daa601724e97c541ea8c9a95af47ea551ab
JPedroBorges <joao.pedro.borges@hotmail.com>
2016-11-04 02:30:22 +0000

    read from configuration file with comments

--- util_simulador.c
+++ util_simulador.c
@@ -19,7 +19,7 @@ int * read_method(){
 	int value;
 	char param[23];
 
-	while(fscanf(configuration_file, "%s : %d", param, &value)!=EOF){
+	while(fscanf(configuration_file, "%s : %d // %*[^\n]s", param, &value)!=EOF){
 		if(!strcmp("simulation_time",param))retorna[0] = value;
 		else if(!strcmp("simulation_population",param))retorna[1] = value;
 		else if(!strcmp("aquapark_start",param))retorna[2] = value;
@@ -30,8 +30,9 @@ int * read_method(){
 		else if(!strcmp("person_max_waiting_time",param))retorna[7] = value;
 		else if(!strcmp("family_chance",param))retorna[8] = value;
 		else if(!strcmp("couple_chance",param))retorna[9] = value;
-		else printf("Error: Not a valid configuration file\n");
+		else printf("Error: %s Not a valid configuration file\n", param);
 	}
+
 	fclose(configuration_file);
 	return retorna;
 }
